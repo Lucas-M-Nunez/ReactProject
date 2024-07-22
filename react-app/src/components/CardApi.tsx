e688134739f579b7d0f958df7754e92874bcf542
Lucas M. Nuñez <lucas_nunez1998@outlook.es>
2024-07-22 14:13:27 -0300

    feat(link card): add new prop link (#143)
    se agrego una propiedad link al FakeApi

--- react-app/src/components/CardApi.tsx
+++ react-app/src/components/CardApi.tsx
@@ -4,6 +4,7 @@ type dataType = {
     name: string,
     details: string,
     img: string;
+    link: string
 }
 interface CardApiProps {
     data: dataType[];
@@ -13,25 +14,29 @@ function CardApi({ data }: CardApiProps) {
     return (
         <>
             {data.map((e) => (
-                    <>
-                        <div className="col">
-                            <div className="card shadow-sm">
-                                <img key={e.id} src={e.img} alt="Image-spider" className="bd-placeholder-img card-img-top" width="100%" height="225" />
-                                <div className="card-body">
-                                    <h1 key={e.name} className="card-title">{e.name}</h1>
-                                    <p key={e.details} className="card-text">{e.details}</p>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <div className="btn-group">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                        </div>
-                                        <small className="text-body-secondary">9 mins</small>
+                <>
+                    <div className="col">
+                        <div className="card shadow-sm">
+                            <img key={e.id} src={e.img} alt="Image-spider" className="bd-placeholder-img card-img-top" width="100%" height="225" />
+                            <div className="card-body">
+                                <h1 key={e.name} className="card-title">{e.name}</h1>
+                                <p key={e.details} className="card-text">{e.details}</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="btn-group">
+                                        <a href={e.link} target="_blank">
+                                            <button type="button" className="btn btn-sm btn-outline-secondary">
+                                                + info
+                                            </button>
+                                        </a>
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">Ver</button>
                                     </div>
+                                    <small className="text-body-secondary">9 mins</small>
                                 </div>
                             </div>
                         </div>
-                    </>
-                ))
+                    </div>
+                </>
+            ))
             }
         </>
     )
--- react-app/src/components/FakeApi.tsx
+++ react-app/src/components/FakeApi.tsx
@@ -6,54 +6,63 @@ export function FakeApi() {
           name: 'Spider-man Tobey',
           details: 'Spiderman',
           img: "./imgs/img-4.png",
+          link: ''
         },
         { 
           id: 2,
           name:'Spider-man Miles Morales',
           details: 'Spiderman Tierra-1610',
           img: './imgs/img-3.png',
+          link: ''
         },
         { 
           id: 3,
           name:'Spider-man Insomniac Games',
           details: 'Spiderman',
           img: './imgs/img-2.png',
+          link: ''
         },
         { 
           id: 4,
           name:'Spider-man Andrew',
           details: 'Spiderman version de Andrew Garfield',
           img: './imgs/img-1.png',
+          link: ''
         },
         { 
           id: 5,
           name:'Spider Gwen',
           details: 'Spiderman across the spiderverse Tierra-65',
           img: './imgs/img-gwen.png',
+          link: ''
         },
         { 
           id: 6,
           name:'Spot',
           details: 'Spot across the spiderverse villano',
           img: './imgs/img-spot.png',
+          link: ''
         },
         { 
           id: 7,
           name:'Kraven',
           details: 'Kraven el Cazador (Sergei Krávinov) es un supervillano Ruso que aparece en los cómics estadounidenses publicados por Marvel Comics. Debutando en el número 15 de The Amazing Spider-Man (agosto de 1964) como el enemigo del Hombre Araña pero a veces como antihéroe. Desde entonces ha resistido como uno de los enemigos más formidables del lanzaredes y es parte del colectivo de adversarios que conforman la galería de villanos del Hombre Araña. Sergei también ha entrado en conflicto con otros héroes, como La Pantera Negra y Tigra. Sergei es el hermanastro y enemigo del Camaleón y uno de los miembros fundadores de Los Seis Siniestros.',
           img: './imgs/img-kraven.png',
+          link: 'https://es.wikipedia.org/wiki/Kraven_el_Cazador'
         },
         { 
           id: 8,
           name:'Venom',
           details: 'Villano Marvel Spiderman 2 PS5',
           img: './imgs/img-venom.png',
+          link: ''
         },
         { 
           id: 9,
           name:'Lizard',
           details: 'Dr. Curtis "Curt" Connors - Villano Marvel Spiderman 2 PS5',
           img: './imgs/img-lizard.png',
+          link: ''
         }
       ];
 
